f24b25962d9ee37b59abe240af6f95e1f9831996
sck123 <ck_shen@163.com>
2018-07-08 04:50:58 -0400

    test rebase second step

--- vulacn.py
+++ vulacn.py
@@ -2,4 +2,12 @@ print "Hello World !"
 a 10,
 b 5,
 print a,
-
+def text_create():
+    path='j/data/learn/git/python/'
+    for text_name in name(1,10):
+        #1-10的范围需要用到range函数
+        with open (path + str(text_name) + '.txt', 'w') as text:
+            text.write(str(txtx_name))
+            text.close()
+            print('done')
+            text_create()
